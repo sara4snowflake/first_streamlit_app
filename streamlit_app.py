2f1ba045f734d8eb91d136bb87acc9e1eac36a8f
sara4snowflake <128649633+sara4snowflake@users.noreply.github.com>
2023-03-22 21:58:06 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import  streamlit
 streamlit.title("My Parents New Healthy Diner")
-sreamlit.header("Breakfast Menu")
+streamlit.header("Breakfast Menu")
 streamlit.text("Omega 3 & Blueberry OAtmeal")
 streamlit.text("Kale, Spinach & Rocket Smoothie")
 streamlit.text("Hard Boiled Free Range Egg")               
